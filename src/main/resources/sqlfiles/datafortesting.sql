5d83efe20a61c1bad71002dff9d6a86567bff0d6
Tenshi <tenshikhimari@gmail.com>
2016-04-12 14:27:59 +0200

    files for testing app

---
+++ src/main/resources/sqlfiles/datafortesting.sql
@@ -0,0 +1,6 @@
+create table test (testpk number(5) primary key,
+                   testvarchar varchar2(255),
+                   testnumber number(100),
+                   testlong long,
+                   testdate  date;
+
