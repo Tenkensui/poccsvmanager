db820f1068b106cac41b7994a35dbdf4943d40b2
smallpawn <smallpawn@gmail.com>
2016-04-13 22:12:34 +0200

    Refartor

--- src/main/java/com/moatcrew/dynamicforms/services/JsonConverterService.java
+++ src/main/java/com/moatcrew/dynamicforms/services/JsonConverterService.java
@@ -1,9 +1,16 @@
 package com.moatcrew.dynamicforms.services;
 
+import org.json.JSONArray;
+
 /**
  * Created by maruku on 13/04/16.
  */
-public interface JsonConverterService<T> {
+public interface JsonConverterService {
 
-    T convertToJson(String inputFilePath);
+    /**
+     * Converts an input file to a JSONArray
+     * @param inputFilePath path to the file to convert
+     * @return a JSONArray representing the innput file content
+     */
+    JSONArray convertToJson(String inputFilePath);
 }
--- src/main/java/com/moatcrew/dynamicforms/services/SqlToJsonConverterService.java
+++ src/main/java/com/moatcrew/dynamicforms/services/SqlToJsonConverterService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
  * Created by maruku on 13/04/16.
  */
 @Service
-public class SqlToJsonConverterService implements JsonConverterService<JSONArray> {
+public class SqlToJsonConverterService implements JsonConverterService {
 
     public JSONArray convertToJson(String inputFilePath) {
         return null;
