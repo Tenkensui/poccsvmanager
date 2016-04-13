db49078fa29b27e4fb15d09e89ffd8cad63c74b0
smallpawn <smallpawn@gmail.com>
2016-04-13 21:52:04 +0200

    Very Simple Sample (vss) form generation from a spring controller json (sample spring controller)

---
+++ src/main/java/com/moatcrew/csvmanager/controllers/DynamicFormsController.java
@@ -0,0 +1,46 @@
+package com.moatcrew.csvmanager.controllers;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by maruku on 12/04/16.
+ */
+@Controller
+public class DynamicFormsController {
+
+    @RequestMapping("/")
+    public String helloWorld(Model model) {
+
+        return "/services/sample.html";
+    }
+
+    @RequestMapping(value = "/getSampleJson", produces = "application/json")
+    @ResponseBody
+    public String getSampleJson() {
+        JSONObject email = new JSONObject();
+        email.put("type", "email");
+        email.put("model", "email");
+        email.put("label", "email");
+        email.put("placeholder", "email");
+
+        JSONObject number = new JSONObject();
+        number.put("type", "number");
+        number.put("model", "number");
+        number.put("label", "number");
+        number.put("placeholder", "number");
+
+        JSONArray ja = new JSONArray();
+        ja.put(email);
+        ja.put(number);
+
+        return ja.toString();
+    }
+
+
+}
