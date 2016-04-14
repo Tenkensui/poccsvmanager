97b0a55da647496adf54a84869777db379ec5091
smallpawn <smallpawn@gmail.com>
2016-04-14 13:43:28 +0200

    Fix

--- src/main/java/com/moatcrew/dynamicforms/controllers/DynamicFormsController.java
+++ src/main/java/com/moatcrew/dynamicforms/controllers/DynamicFormsController.java
@@ -4,7 +4,6 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
