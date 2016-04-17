c23dc2b5046a2ff36bcad9e340b1312a530ff99e
smallpawn <smallpawn@gmail.com>
2016-04-17 17:29:23 +0200

    Create new record

--- src/main/java/com/moatcrew/dynamicforms/services/CsvDataService.java
+++ src/main/java/com/moatcrew/dynamicforms/services/CsvDataService.java
@@ -5,11 +5,17 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 /**
@@ -21,15 +27,47 @@ public class CsvDataService {
     private Logger LOG = Logger.getLogger(CsvDataService.class.getName());
 
     public JSONArray find(final String form) {
-        File[] matchingFiles = csvFilesDirectory.listFiles(new FilenameFilter() {
+        File csvFile = getCsvFile(form);
+        if (csvFile != null) {
+            return fileToJson(csvFile);
+        }
+        return new JSONArray();
+    }
+
+    public String create(String form, Map<String, Object> dataMapping) {
+        File csvFile = getCsvFile(form);
+        String uuid = UUID.randomUUID().toString();
+        if (csvFile != null) {
+            try {
+                dataMapping.put("uuid", uuid);
+                String[] headers = getHeaders(csvFile);
+                String line = "\n";
+                for (String header : headers) {
+                    line += dataMapping.get(header) + "|";
+                }
+                // Remove last pipe
+                line = line.replaceFirst("\\|$", "");
+                Files.write(Paths.get(csvFile.toURI()), line.getBytes(), StandardOpenOption.APPEND);
+                return uuid;
+            } catch (IOException e) {
+                LOG.severe(e.getMessage());
+            }
+        }
+        return "";
+    }
+
+    File getCsvFile(final String form) {
+        File[] files = csvFilesDirectory.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return name.startsWith(form + ".") && name.endsWith("csv");
             }
         });
-        if (matchingFiles.length > 0) {
-            return fileToJson(matchingFiles[0]);
-        }
-        return new JSONArray();
+        return files.length > 0 ? files[0] : null;
+    }
+
+    private String[] getHeaders(File file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        return br.readLine().split("\\|");
     }
 
     private JSONArray fileToJson(File matchingFile) {
--- src/test/java/com/moatcrew/dynamicforms/services/CsvDataServiceTest.java
+++ src/test/java/com/moatcrew/dynamicforms/services/CsvDataServiceTest.java
@@ -5,6 +5,16 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+
 /**
  * Created by maruku on 17/04/16.
  */
@@ -22,5 +32,21 @@ public class CsvDataServiceTest {
         JSONArray jsonArray = service.find("test");
         Assert.assertNotNull(jsonArray);
         Assert.assertTrue(jsonArray.length() > 0);
+    }
+
+    @Test
+    public void createTest() throws Exception {
+        Map<String, Object> dataMapping = new HashMap<>();
+        dataMapping.put("testpk1", "newPk");
+        dataMapping.put("testpk2", "newPk2");
+        dataMapping.put("testvarchar", "newValue");
+        dataMapping.put("testnumber", 12);
+        dataMapping.put("testdate", "2015-05-29");
+        dataMapping.put("template", "template");
+        String uuid = service.create("test", dataMapping);
+        String expected = uuid + "|newPk|newPk2|newValue|12|2015-05-29|template";
+        File csvFile = service.getCsvFile("test");
+        List<String> contents = Files.readAllLines(Paths.get(csvFile.toURI()), Charset.forName("utf8"));
+        Assert.assertEquals(expected, contents.get(contents.size() - 1));
     }
 }
